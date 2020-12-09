30bab07e2787246d479c9733de28935187764553
Dillon Sumner <56370697+DillonSumnerEpicGamer@users.noreply.github.com>
2020-12-09 15:12:52 -0800

    Lesson 4.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject powerUpIndicator;
+
     private float speed = 3.0f;
+    private float powerUpStrength = 15.0f;
     private Rigidbody playerRigidbody;
     private GameObject focalPoint;
+    private bool hasPowerUp = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +24,36 @@ public class PlayerController : MonoBehaviour
     {
         float forewardInput = Input.GetAxis("Vertical");
         playerRigidbody.AddForce(focalPoint.transform.forward * forewardInput * speed);
+
+        powerUpIndicator.transform.position = transform.position + new Vector3(0,-.5f,0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            powerUpIndicator.gameObject.SetActive(true);
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerUpIndicator.gameObject.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody EnemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.transform.position - transform.position);
+
+            EnemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+        }
     }
 }
